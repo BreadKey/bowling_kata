25b1159c5a1f24e23921db10274b4bc69c62b8c3
이영기 <youngkey.lee@navercorp.com>
2018-07-28 00:09:24 +0900

    20번 모두 1점수를 낸 테스트케이스 작성

--- tests/BowlingGameTest.java
+++ tests/BowlingGameTest.java
@@ -11,4 +11,13 @@ public class BowlingGameTest {
         }
         assertEquals(0, bowlingGame.score());
     }
+
+    @Test
+    public void testAllOnes() {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(1);
+        }
+        assertEquals(20, bowlingGame.score());
+    }
 }
