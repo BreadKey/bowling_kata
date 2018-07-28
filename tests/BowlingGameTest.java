e89fa6d82683e76e58ec9551c5081c53aa47234c
이영기 <youngkey.lee@navercorp.com>
2018-07-28 22:37:08 +0900

    스페어 테스트케이스 작성

--- tests/BowlingGameTest.java
+++ tests/BowlingGameTest.java
@@ -25,6 +25,15 @@ class BowlingGameTest {
         assertEquals(20, bowlingGame.score());
     }
 
+    @Test
+    public void testOneSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5); // 스페어!
+        bowlingGame.roll(3);
+
+        assertEquals(16, bowlingGame.score());
+    }
+
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             bowlingGame.roll(pins);
