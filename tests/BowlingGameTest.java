93c4e31b746977c87b6d09b7d99e3a3ce10bb9f1
이영기 <youngkey.lee@navercorp.com>
2018-07-28 22:35:35 +0900

    bowlingGame은 매 테스트 마다 초기화 되므로 before로 초기화

--- tests/BowlingGameTest.java
+++ tests/BowlingGameTest.java
@@ -1,27 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @BeforeEach
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void testGutterGame() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        rollMany(bowlingGame, 20, 0);
+        rollMany(20, 0);
 
         assertEquals(0, bowlingGame.score());
     }
 
     @Test
     public void testAllOnes() {
-        BowlingGame bowlingGame = new BowlingGame();
-
-        rollMany(bowlingGame, 20, 1);
+        rollMany(20, 1);
 
         assertEquals(20, bowlingGame.score());
     }
 
-    private void rollMany(BowlingGame bowlingGame, int n, int pins) {
+    private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             bowlingGame.roll(pins);
         }
