55d39cf7b118ee2a46252d351aa392aa9f6a778e
이영기 <youngkey.lee@navercorp.com>
2018-07-30 21:19:29 +0900

    퍼펙트게임 테스트 추가

--- tests/BowlingGameTest.java
+++ tests/BowlingGameTest.java
@@ -44,6 +44,12 @@ class BowlingGameTest {
         assertEquals(24, bowlingGame.score());
     }
 
+    @Test
+    public void testPerfectGame() {
+        rollMany(12, 10);
+        assertEquals(300, bowlingGame.score());
+    }
+
     private void rollMany(int n, int pins) {
         for (int i = 0; i < n; i++) {
             bowlingGame.roll(pins);
