5a7ddfc0dda688285314889e7f907feabde599c7
이영기 <youngkey.lee@navercorp.com>
2018-07-30 21:17:56 +0900

    보너스 점수 로직 분리하여 가독성 높힘

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -12,19 +12,19 @@ public class BowlingGame {
         int frameIndex =0;
         for (int frame = 0; frame < 10; frame++) {
             if (isStrike(frameIndex)) {
-                score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+                score += 10 + strikeBonus(frameIndex);
 
                 frameIndex += 1;
             }
 
             else if (isSpare(frameIndex)) {
-                score += 10 + rolls[frameIndex + 2];
+                score += 10 + spareBonus(frameIndex);
 
                 frameIndex += 2;
             }
 
             else {
-                score += rolls[frameIndex] + rolls[frameIndex + 1];
+                score += sumOfBallsInFrame(frameIndex);
 
                 frameIndex += 2;
             }
@@ -37,5 +37,17 @@ public class BowlingGame {
 
     private boolean isSpare(int frameIndex) {
         return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+    }
+
+    private int sumOfBallsInFrame(int frameIndex) {
+        return rolls[frameIndex] + rolls[frameIndex + 1];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return rolls[frameIndex + 1] + rolls[frameIndex + 2];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return rolls[frameIndex + 2];
     }
 }
